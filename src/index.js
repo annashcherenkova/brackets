7c64357840d312b9022e6c31e02ceda9c14ea99f
annashcherenkova <a.shcherenkova@gmail.com>
2022-08-13 02:10:46 +0300

    feat: Add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+module.exports = 
+function check(str, bracketsConfig) {
+  let copyBC = [].concat(bracketsConfig);
+  let strBracket = '';
+  let findBracket = false; 
+  for (let index = 0; index < copyBC.length; index++) {
+    const element = copyBC[index];
+    strBracket = element[0] + element[1];
+      while (str.indexOf(strBracket) != -1) {
+        str = str.replace(strBracket,'');
+        findBracket = true;
+      }
+      if (findBracket) {          
+        index = -1;
+        findBracket = false;
+      };
+  }
+  return str.length === 0;
 }
+
